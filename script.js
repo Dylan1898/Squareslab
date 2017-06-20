44eecdbdd5d8cd56c7ab7e76b0b1a62b393d73f3
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-20 14:14:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+var i=-1
+        
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var button= document.createElement('button');
+    document.body.appendChild(button)
+    button.addEventListener("click", function () {
+        var square = document.createElement('div');
+        square.className='squares';
+        for (i; i<square;){
+        };
+        i++;
+        square.id=[i]
+        // square.addEventListener('mouseover', function(){
+        //     square.innerText = square.id;
+        // });
+         square.addEventListener('mouseout', function(){
+            square.innerText= '';
+         });
+         square.addEventListener('click', function(){
+            //  console.log ('works')
+            var backColor =["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+                var rand = backColor[Math.floor(Math.random() * backColor.length)];
+               console.log(rand)
+               square.style.backgroundColor=(rand);
+         })
+         square.addEventListener('dblclick', function(){
+                 if( square.id % 2===0){
+                //    square.nextSibling;
+                
+                    square.remove('nextElementSibling');
+                    console.log('works')
+        }
+        // else{
+
+
+
+        // }
+         })
+         
+        document.body.appendChild(square)
+
+
+
+    });
+});
